c3e4b00ed96e5334e155eb3aa9ddd66500b6d194
Erick-Marinho <erickgaves@gmail.com>
2021-08-07 17:26:40 -0300

    Excluindo arquivo criado no dir raiz

--- desafio4.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df_company = pd.read_csv('data/DadosEmpresa.csv')
-
-Sum_Social_Capital = "R$ {:_.2f}".format(
-    df_company['capital_social']
-    .sum()).replace('_', '.')
-
-print(Sum_Social_Capital)
